32f2b1a38332bdf5c2f4e916cb51b4bd7909304e
Nikolai Saganenko <9952728@gmail.com>
2021-10-28 15:19:35 +0300

    Initial Commit

---
+++ Whitehouse Petitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Whitehouse Petitions
+//
+//  Created by Nick Sagan on 28.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
