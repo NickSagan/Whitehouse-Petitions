b54ad613524c9717db90cb5373480af3d401b906
Nikolai Saganenko <9952728@gmail.com>
2021-10-28 15:35:40 +0300

    Add UITableViewController Add UINavBarController

---
+++ Whitehouse Petitions/Petition.swift
@@ -0,0 +1,8 @@
+//
+//  Petition.swift
+//  Whitehouse Petitions
+//
+//  Created by Nick Sagan on 28.10.2021.
+//
+
+import Foundation
--- Whitehouse Petitions/ViewController.swift
+++ Whitehouse Petitions/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var petitions = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        petitions = ["fsdfsd", "dsfsdfs"]
+        
     }
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here..."
+        return cell
+    }
 
 }
 
